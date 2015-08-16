92cabd107b4ac6ccae5f546f1bae414c64db1058
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2015-08-16 17:03:45 +0200

    paths, links, bla

--- Unit.cpp
+++ Unit.cpp
@@ -1,17 +1,5 @@
 #include "Unit.h"
 
-/*class Unit{
-public:
-	int maxAP;
-	int currAP;
-	int maxHP;
-	int currHP;
-	sf::Sprite sprite;
-
-	Unit(int, int, sf::Sprite);
-};*/
-	
-
 	Unit::Unit(int mAP, int mHP, sf::Sprite s)
 	{
 		maxAP = mAP;
