8a7e34dd68e2fe029f06525328f99413dbc20b9e
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2015-08-16 14:44:19 +0200

    enabled closing with escape

--- Quelle.cpp
+++ Quelle.cpp
@@ -11,7 +11,7 @@ int main()
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+			if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 				window.close();
 		}
 
