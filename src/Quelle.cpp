36cec05ffc8589634626a8e76a0e87567c83994a
Philipp Wimmer <pwimmer.mmt-b2014@fh-salzburg.ac.at>
2015-08-18 15:34:38 +0200

    graphics added

--- src/Quelle.cpp
+++ src/Quelle.cpp
@@ -54,8 +54,8 @@ int main() {
 
 	lvlManager lvl;
 	Block b = lvl.generateTiles(lvl.buildKey(&texMng), &texMng);
-	lvl.genMap(b, 0, 0);
-	for (int i = 0; i < 10; i++){
+	lvl.genMap(b, 0, 15);
+	for (int i = 0; i < 25; i++){
 		Block b = lvl.generateTiles(lvl.buildKey(&texMng), &texMng);
 		sf::Vector2<int> v = lvl.findNextSpot(b.x, b.y);
 		lvl.genMap(b, v.x, v.y);
